d7c3fc7b99690639cc1b1e58216d3ef9665cc153
Mikhail Ivannikov <m.ivannikof@gmail.com>
2020-05-22 22:25:52 +0300

    fix: Remove trailing slash from url

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import { setBaseUrl } from '../../src'
+import { setBaseUrl } from '../../src/'
 import { ROUTES } from './constants/routes'
 
 import Users from './components/pages/Users'
 import User from './components/pages/User'
 import CreateUser from './components/pages/CreateUser'
 
-const baseUrl = 'https://5eb2deca974fee0016ecd021.mockapi.io'
+const baseUrl = 'https://5eb2deca974fee0016ecd021.mockapi.io/'
 
 const App: React.FC = () => {
   React.useMemo(() => setBaseUrl(baseUrl), [])
