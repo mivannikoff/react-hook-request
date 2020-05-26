75f418c96d3d8837b4bc2a8a28aef6791f3dc2e2
Mikhail Ivannikov <m.ivannikof@gmail.com>
2020-05-26 16:11:12 +0300

    feat: Add global error config

--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from 'axios'
+
 type Headers = {
   [key: string]: string
 }
@@ -6,12 +8,14 @@ export interface IGlobalConfig {
   baseUrl: string
   headers: Headers
   showConsoleError?: boolean
+  globalErrorCallback: (error: AxiosError) => void
 }
 
 const globalConfig: IGlobalConfig = {
   baseUrl: window.origin,
   headers: {},
   showConsoleError: true,
+  globalErrorCallback: () => {},
 }
 
 export default globalConfig
